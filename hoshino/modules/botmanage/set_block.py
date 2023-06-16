7a8085fe0eb92ed389acf8f4d9cd6a35562b26a1
Rinco304 <75192177+Rinco304@users.noreply.github.com>
2023-06-17 02:34:16 +0800

    Create set_block.py

---
+++ hoshino/modules/botmanage/set_block.py
@@ -0,0 +1,28 @@
+import hoshino
+import re
+from datetime import timedelta
+from hoshino import Service, priv
+from hoshino.typing import CQEvent
+
+sv = Service('设置拉黑', visible=False)
+
+@sv.on_prefix('拉黑')
+async def set_block(bot, ev: CQEvent):
+    if not priv.check_priv(ev, priv.SUPERUSER):
+        return
+    content = ev.message.extract_plain_text().strip()
+    try:
+        if not priv.check_priv(ev, priv.SUPERUSER):
+            return
+        match = re.match(r'(\d+)\s+(.+)', content)
+        if match:
+            user_id = match.group(1)
+            hours = match.group(2)
+        else:
+            await bot.send(ev, "输入格式不正确，QQ号与封禁类型之间使用空格分隔,qq号后跟上要拉黑的时间(单位为小时)")
+            return
+    except Exception as e:
+        await bot.send(ev, '出现异常' + str(e))
+    else:
+        hoshino.priv.set_block_user(int(user_id), timedelta(int(hours)))
+        await bot.send(ev,f"拉黑 {user_id} 成功，拉黑时间为{hours}小时")
